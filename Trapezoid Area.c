fdd973058ab23888c6cf1461bf1a4223766ab89b
amanver02 <155369942+amanver02@users.noreply.github.com>
2024-02-25 00:26:59 +0530

    Add files via upload

---
+++ Trapezoid Area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    float base1, base2, height, area;
+
+    printf("Enter the lengths of two bases and height :");
+    scanf("%f %f %f", &base1, &base2, &height);
+
+    area = 0.5 * (base1 + base2) * height;
+
+    printf("Area of the trapezoid : %.2f/n", area);
+    return 0;
+}
