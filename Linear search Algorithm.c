b30ad5b86390d1a7fe67d7a1602b1fc699c9743e
amanver02 <155369942+amanver02@users.noreply.github.com>
2024-03-07 17:16:14 +0530

    Create Linear search Algorithm.c

---
+++ Linear search Algorithm.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int linear_search(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;  // Return the index of the target element if found
+        }
+    }
+    return -1;  // Return -1 if the target element is not in the array
+}
+
+int main() {
+    int myArray[] = {2, 4, 6, 8, 10, 12, 14};
+    int arraySize = sizeof(myArray) / sizeof(myArray[0]);
+    int targetElement = 8;
+
+    int result = linear_search(myArray, arraySize, targetElement);
+
+    if (result != -1) {
+        printf("Target element %d found at index %d.\n", targetElement, result);
+    } else {
+        printf("Target element %d not found in the array.\n", targetElement);
+    }
+
+    return 0;
+}
