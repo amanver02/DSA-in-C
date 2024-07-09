1121118d55ce2a353c3d16d0ea51b9bc90bd8984
Aman Verma <155369942+amanver02@users.noreply.github.com>
2024-07-09 20:09:17 +0530

    Trapezoid Area.c

--- Trapezoid Area.c
+++ Trapezoid Area.c
@@ -7,7 +7,7 @@ int main(){
     scanf("%f %f %f", &base1, &base2, &height);
 
     area = 0.5 * (base1 + base2) * height;
-
+  
     printf("Area of the trapezoid : %.2f/n", area);
     return 0;
 }
